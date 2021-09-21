4dbbec92b2c4e5eb91b3641f9c05b438ff9ec93a
Jan Werder <jan.werder@nordakademie.org>
2021-09-21 16:32:27 +0200

    First steps towards MouseFlight

--- Player.gd
+++ Player.gd
@@ -4,20 +4,20 @@ export var speed = 15
 export var rot_speed = 0.85
 
 var velocity = Vector3.ZERO
+onready var universe_center = get_tree().get_root().get_node("/root/Main/Universe_Center")
+onready var ui_mousePosAim  = get_tree().get_root().get_node("/root/Main/HUD/mousePosAim")
+onready var ui_mouseShouldAim  = get_tree().get_root().get_node("/root/Main/HUD/mouseShouldAim")
+onready var debug_geometry  = get_tree().get_root().get_node("/root/Main/DebugGeometry")
 
+#Position where the Player moves towards
+var mousePosAim
+#Position where the Player should move towards
+var mouseShouldAim
+#2D Vector between mousePosAim and mouseShouldAim
+var move_2d
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-
+	pass
 
 func _physics_process(delta):
 	get_input(delta)
@@ -25,6 +25,36 @@ func _physics_process(delta):
 	
 	$gravity.look_at(Vector3(0,0,0),Vector3.UP)
 	
+	mousePosAim = $MoveTowards.global_transform
+	
+	ui_mousePosAim.rect_position.x =  $Camera.unproject_position(mousePosAim.origin).x - ( ui_mousePosAim.rect_size.x / 2 ) 
+	ui_mousePosAim.rect_position.y =  $Camera.unproject_position(mousePosAim.origin).y - ( ui_mousePosAim.rect_size.y / 2 )
+	
+	ui_mouseShouldAim.rect_position.x = get_viewport().get_mouse_position().x - ( ui_mouseShouldAim.rect_size.x / 2 ) 
+	ui_mouseShouldAim.rect_position.y = get_viewport().get_mouse_position().y - ( ui_mouseShouldAim.rect_size.y / 2 )
+	
+	move_2d = (ui_mouseShouldAim.rect_position - ui_mousePosAim.rect_position).normalized()
+	
+	#debug_geometry.clear()
+	#debug_geometry.begin(PrimitiveMesh.PRIMITIVE_LINES)
+	#debug_geometry.set_color(Color(1,1,1))
+	#debug_geometry.add_vertex(self.transform.origin) 
+	#debug_geometry.add_vertex(mousePosAim.origin) 		
+	#debug_geometry.end()
+	
+	#if universe_center:
+		#Vector between the ship and the UniverseCenter. Normalized because we only care about the direction
+	#	var gravity_direction = (universe_center.global_transform.origin - global_transform.origin)
+		
+	#	#Debug
+	#	debug_geometry.clear()
+	#	debug_geometry.begin(PrimitiveMesh.PRIMITIVE_LINES)
+	#	debug_geometry.set_color(Color(1,1,1))
+	#	debug_geometry.add_vertex(self.transform.origin) 
+	#	debug_geometry.add_vertex(self.transform.origin + gravity_direction) 		
+	#	debug_geometry.end()
+		
+	#	$Plane.look_at(gravity_direction.normalized(),Vector3.UP)
 	
 	#self.global_transform.basis.slerp($gravity.global_transform.basis,delta*89)
 #	print(Quat($gravity.global_transform.basis))
@@ -39,7 +69,26 @@ func _physics_process(delta):
 	#https://godotengine.org/qa/57911/how-to-lerp-rotation-in-godot-smooth-rotate-back-to-zero
 	#Matrizen dreh matrix
 	
+var pitch_input = 0
+var roll_input = 0	
+var yaw_input = 0
+export var input_response = 8.0
+export var pitch_speed = 0.5
+export var roll_speed = 0.9
+export var yaw_speed = 0.5
+
 func get_input(delta):
+	
+	if move_2d:
+		pitch_input = lerp(pitch_input, move_2d.y * -1, input_response * delta)
+		#roll_input = lerp(roll_input, move_2d.x * -1, input_response * delta)
+		yaw_input = lerp(yaw_input, move_2d.x * -1, input_response * delta)
+		
+		#transform.basis = transform.basis.rotated(transform.basis.z, roll_input * roll_speed * delta)
+		transform.basis = transform.basis.rotated(transform.basis.x, pitch_input * pitch_speed * delta)
+		transform.basis = transform.basis.rotated(transform.basis.y, yaw_input * yaw_speed * delta)
+		transform.basis = transform.basis.orthonormalized()
+	
 	var vy = velocity.y
 	velocity = Vector3.ZERO
 	if Input.is_action_pressed("forward"):
